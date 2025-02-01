3de27c4faa907040f0b73c18a13da8b7c91e6c51
Subhradeep1708 <subhradeep1708@gmail.com>
2025-02-01 19:39:26 +0530

    initialize project with Vite and React setup, including basic components and configuration files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navigations from "./components/Navigations"
+import './App.css'
+import Hero from "./components/Hero"
+
+const App = () => {
+  return (
+    <div>
+     <Navigations/>
+     <Hero/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navigations.jsx
@@ -0,0 +1,23 @@
+const Navigations = () => {
+    return (
+        <div>
+            <nav className="container">
+
+                <div className="logo">
+                    <img src="./public/brand_logo.png" alt="brand_logo" />
+                </div>
+
+                <ul>
+                    <li href="#">MENU</li>
+                    <li href="#">LOCATION</li>
+                    <li href="#">ABOUT</li>
+                    <li href="#">CONTACT</li>
+                </ul>
+
+                <button>login</button>
+            </nav>
+        </div>
+    )
+}
+
+export default Navigations
